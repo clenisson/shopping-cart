277c0923ff2a19b18db3bf65515cd96c38e88b82
Clenisson <clenisson.calaca@gmail.com>
2025-05-19 02:40:31 -0300

    Product CRUD

--- app/Http/Controllers/GraphQLController.php
+++ app/Http/Controllers/GraphQLController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use GraphQL\GraphQL;
 use GraphQL\Utils\BuildSchema;
 use App\Models\Client;
+use App\Models\Product;
 use GraphQL\Error\DebugFlag;
 
 class GraphQLController extends Controller
@@ -44,6 +45,33 @@ class GraphQLController extends Controller
         },
         'deleteClient' => function ($root, array $args, $ctx, $info) {
             return (bool) Client::destroy($args['id']);
+        },
+
+        'products' => function ($root, array $args, $ctx, $info) {
+            return Product::all();
+        },
+
+        'product' => function ($root, array $args, $ctx, $info) {
+            return Product::find($args['id']);
+        },
+
+        'createProduct' => function ($root, array $args, $ctx, $info) {
+            return Product::create([
+                'name'  => $args['name'],
+                'price' => $args['price'],
+            ]);
+        },
+        'updateProduct' => function ($root, array $args, $ctx, $info) {
+            $product = Product::findOrFail($args['id']);
+            $product->fill(array_filter([
+                'name'  => $args['name']  ?? null,
+                'price' => $args['price'] ?? null,
+            ], fn($v) => ! is_null($v)));
+            $product->save();
+            return $product;
+        },
+        'deleteProduct' => function ($root, array $args, $ctx, $info) {
+            return (bool) Product::destroy($args['id']);
         },
     ];
 
